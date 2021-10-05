35780a58b265fef77fd674e618b9f2af081ec0b5
nimesh garia <nimesh.garia@gmail.com>
2021-10-05 11:34:22 +0530

    added settext

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         topText = (TextView) findViewById(R.id.topTextView);
-        topText.setText("");
+        topText.setText("hello");
 
         // Step 1 : create Retrofit object
         Retrofit retrofit = new Retrofit.Builder()
